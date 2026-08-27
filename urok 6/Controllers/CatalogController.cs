7b9a4966354c217842ddef246c7d539d341aee39
Lev-Dobrianskyi <lionnametu@gmail.com>
2026-08-27 19:00:56 +0300

    Add project files.

---
+++ urok 6/Controllers/CatalogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace urok_6.Controllers
+{
+    [Route("catalog/{category}/filter")]
+    public class CatalogController : Controller
+    {
+        [HttpGet]
+        public IActionResult Filter(string category, decimal minPrice, decimal maxPrice)
+        {
+            ViewBag.Category = category;
+            ViewBag.MinPrice = minPrice;
+            ViewBag.MaxPrice = maxPrice;
+
+            return View();
+        }
+    }
+}
